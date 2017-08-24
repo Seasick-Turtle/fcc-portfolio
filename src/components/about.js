0a4241367b069f828978b99cb68f09558adae0ab
Christopher Martin <martincdev@gmail.com>
2017-08-24 18:30:37 -0500

    Refine about component content

--- src/components/about.js
+++ src/components/about.js
@@ -17,21 +17,24 @@ export default class About extends Component {
                 Hello there. My name is Christopher Martin and I'm an aspiring web developer. I recently graduated from Columbia College and finally earned my degree, a Bachelor's of Science in Computer Information Systems.
               </p>
               <p>
-                While at Columbia College I studied C++ and Java, although I enjoyed taking these classes I wanted to do something different.
+                While at Columbia College I studied C++ and Java, although I enjoyed taking these classes I came to realize that I wanted to give web development a shot. Although this happened near the very end of my college experience, I didn't want to go back; web development allows for me to be creative and feel proud of my creations. I would like to share that proud feeling of having something that looks amazing and fits the user's needs.
               </p>
             </Col>
             <Col xs={{ size: 4 }} md="4" lg="4" className="a-div">
               <h3 className="a-title">What do I do? <i className="fa fa-laptop" aria-hidden="true"></i></h3>
               <hr/>
               <p>
-                Currently I'm working on front end web development but eventually I'll learn more about back end technologies.
+                As of right now, I have a basic understanding of both back end development and dealing with databases. Currently, I'm improving my skills with React.JS and other front end technologies.
+              </p>
+              <p>
+                My future goal is to become a skilled full stack engineer with a deep understanding of JavaScript and other technologies so I can implement the proper tools along with great design.
               </p>
             </Col>
             <Col xs={{ size: 4 }} md="4" lg="4" className="a-div">
               <h3 className="a-title">My Toolbox <i className="fa fa-wrench" aria-hidden="true"></i></h3>
               <hr/>
               <p>
-                Wondering what technologies I've used? Here's a list of them :
+                Wondering what technologies I've used? Here's a list of them:
               </p>
               Front end:
               <ul>
