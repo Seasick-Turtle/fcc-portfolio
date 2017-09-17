7468cf9692842ff88c1764811428b6f75db3def2
Christopher Martin <martincdev@gmail.com>
2017-09-17 05:32:54 -0500

    Add state to project information to reduce size of modal

--- src/components/project.js
+++ src/components/project.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import { Card, CardImg, Button, ButtonGroup, Row, Col, Container,
+import { Card, CardImg, Button, Row, Col, Container,
   Modal, ModalHeader, ModalBody, ModalFooter,
 } from 'reactstrap';
-
 import '../styles/project.css';
+import { PropTypes } from 'prop-types';
 
-function renderGitHubButton(props) {
+function ShowGitHubButton(props) {
   return (
     <a href={props.ghLink} target="_blank">
       <Button color="primary">View on GitHub</Button>
@@ -13,7 +13,7 @@ function renderGitHubButton(props) {
   )
 }
 
-function renderHerokuButton(props) {
+function ShowHerokuButton(props) {
   return (
     <a href={props.herokuLink} target="_blank">
       <Button color="primary">View on Heroku</Button>
@@ -21,17 +21,87 @@ function renderHerokuButton(props) {
   )
 }
 
+function ShowDescription(props) {
+  return (
+    <div style={props.selectedDisplay === 'Description' ? { visibility: 'visible'}: {display: 'none'}}>
+      <h5 className="project-desc project-header"> Description: </h5>
+      <p className="project-desc">{props.description}</p>
+    </div>
+  )
+}
+
+function ShowCreated(props) {
+  return (
+    <div style={props.selectedDisplay === 'Created With' ? { visibility: 'visible'}: {display: 'none'}}>
+      <h5 className="project-desc project-header"> Created With: </h5>
+      <p className="project-desc">{props.createdWith}</p>
+    </div>
+  )
+}
+
+function ShowDetails(props) {
+  return (
+    <div style={props.selectedDisplay === 'More Details' ? { visibility: 'visible'}: {display: 'none'}}>
+      <h5 className="project-desc project-header"> Description: </h5>
+      {props.detail.map((details) =>{
+        return <p className="project-details" key={details}>{details}</p>
+      })}
+    </div>
+  )
+}
+
+function SelectDisplay(props) {
+  const choices = ['Description', 'Created With', 'More Details'];
+
+  return (
+    <Container fluid={true}>
+      <Row >
+        {choices.map(function (choice) {
+          return (
+            <Col
+              xs='4'
+              key={choice}
+              className="select-column">
+              <li
+                style={choice === props.selectedDisplay ? { color: 'black', fontWeight: 700}: null}
+                onClick={props.onSelect.bind(null, choice)}
+                className="select-item"
+                >
+                {choice}
+              </li>
+            </Col>
+            )
+          }, this)}
+      </Row>
+    </Container>
+  )
+}
+
+SelectDisplay.PropTypes = {
+  selectedDisplay: PropTypes.string.isRequired,
+  onSelect: PropTypes.func.isRequired
+};
+
 export default class Project extends Component {
   constructor(props) {
     super(props);
     this.state = {
       modal: false,
       collapse: false,
-      active: false
+      selectedDisplay: 'Description'
     };
 
     this.toggle = this.toggle.bind(this);
     this.toggleCollapse = this.toggleCollapse.bind(this);
+    this.updateChoice = this.updateChoice.bind(this);
+  }
+
+  updateChoice(choice) {
+    this.setState(function () {
+      return {
+        selectedDisplay: choice
+      }
+    });
   }
 
   toggle() {
@@ -46,26 +116,18 @@ export default class Project extends Component {
     });
   }
 
-  toggleActive() {
-    const displayText = this.state.active;
-    this.setState({
-      active: !displayText
-    })
-  }
-
   render() {
     const {...post} = this.props;
 
     let gitHubButton;
     if (post.ghLink) {
-      gitHubButton = renderGitHubButton(post);
+      gitHubButton = ShowGitHubButton(post);
     }
 
     let herokuButton;
     if (post.herokuLink) {
-      herokuButton = renderHerokuButton(post);
+      herokuButton = ShowHerokuButton(post);
     }
-
 
     return (
       <div>
@@ -81,22 +143,23 @@ export default class Project extends Component {
             <ModalHeader toggle={this.toggle}>{post.title}</ModalHeader>
             <ModalBody>
               <div className="project-details">
-                <Container className="switchBtns">
-                  <ButtonGroup >
-                    <Button>Description</Button>
-                    <Button>Created With</Button>
-                    <Button>More Info</Button>
-                  </ButtonGroup>
-                </Container>
+                <SelectDisplay
+                  selectedDisplay={this.state.selectedDisplay}
+                  onSelect={this.updateChoice}
+                />
                 <hr/>
-                <h5 className="project-open">Description:</h5>
-                <p>{post.description}</p>
-                <h5 className="project-open">Created with: </h5>
-                <p>{post.createdWith}</p>
-                <h5 className="project-open">More details:</h5>
-                {post.detail.map((details) =>{
-                  return <p key={details}>{details}</p>
-                })}
+                <ShowDescription
+                  selectedDisplay={this.state.selectedDisplay}
+                  {...post}
+                />
+                <ShowCreated
+                  selectedDisplay={this.state.selectedDisplay}
+                 {...post}
+                 />
+                <ShowDetails
+                  selectedDisplay={this.state.selectedDisplay}
+                  {...post}
+                />
                 <p id="note">Please note that viewing these applications on Heroku may take some time as they are deployed with Heroku's free plan and it may take some time for the apps to 'wake up'.</p>
               </div>
             </ModalBody>
